8cb50a00176a8cda2a19ed2e63a0ec4509cad35d
Armin-Muric <armin.muric@utb.ecutbildning.se>
2024-04-13 02:20:30 +0200

    Structure init

---
+++ WebAPI_Assignment/Controllers/CoursesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Assignment.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CoursesController : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok();
+    }
+
+}
---
+++ WebAPI_Assignment/Controllers/SubscribeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Assignment.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SubscribeController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Subscribe()
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Unsubscribe()
+    {
+        return Ok();
+    }
+}
